d58cbf7704b646e4a8663c5c18fef3c68dd2cd0c
abhijay-7 <22bcs002@iitdwd.ac.in>
2024-05-18 19:23:56 +0530

    ES6 fun start

--- src/index.js
+++ src/index.js
@@ -6,3 +6,32 @@ import ReactDOM from 'react-dom';
 const element = <h1>hello react</h1>
 
 ReactDOM.render(element, document.getElementById("root"));
+
+function Hello(){
+for(let i =0; i<5;i++){
+    console.log(i);
+
+}
+// console.log(i);
+}
+
+Hello();
+
+const person={
+    name: 'jamie',
+    age:15,
+    setage(age){
+        this.age=age;
+    },
+    Hello(){
+        console.log("hi");
+    }
+}
+
+person.setage(13);
+console.log(person);
+console.log(this);
+
+const hi = person.Hello;
+
+hi();
