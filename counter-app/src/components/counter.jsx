fbf534f840cdd5cd75fa3d172e6c19a8eb16f112
abhijay-7 <22bcs002@iitdwd.ac.in>
2024-05-19 23:25:20 +0530

    c render

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,26 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Counter extends Component {
-    state = { 
-       count: 0
-     } 
-    render() { 
-        let classes = "badge  m-2 bg-"
-        classes+= (this.state.count===0)? 'warning': 'primary'
+    state = {
+        count: 0,
+
+        tags: ["tag1", "tag2", "tag3"],
+    };
+    render() {
+        let classes = "badge  m-2 bg-";
+        classes += this.state.count === 0 ? "warning" : "primary";
         return (
             <div>
-        <span styles={{font:"Roboto"}}    className={classes}>{this.formatCount()}</span>
-        <button className='btn btn-secondary btn-sm'>incremement</button>
+                <span styles={{ font: "Roboto" }} className={classes}>
+                    {this.formatCount()}
+                </span>
+                <button className="btn btn-secondary btn-sm">
+                    incremement
+                </button>
+                <ul>{this.state.tags.map(tag=><li key={tag}>{tag}</li>)}</ul>
             </div>
-    );
-    
+        );
     }
 
-    formatCount(){
-        const count= this.state.count;
-        return count === 0 ? 'zero':count;
+    formatCount() {
+        const count = this.state.count;
+        return count === 0 ? "zero" : count;
     }
-
 }
- 
+
 export default Counter;
