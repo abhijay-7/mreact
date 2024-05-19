4e84998969bde5cc2738c4ffe9534247ef5c2cc1
abhijay-7 <22bcs002@iitdwd.ac.in>
2024-05-19 23:35:11 +0530

    c render

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -9,22 +9,18 @@ class Counter extends Component {
     render() {
         let classes = "badge  m-2 bg-";
         classes += this.state.count === 0 ? "warning" : "primary";
-        return (
-            <div>
-                <span styles={{ font: "Roboto" }} className={classes}>
-                    {this.formatCount()}
-                </span>
-                <button className="btn btn-secondary btn-sm">
-                    incremement
-                </button>
-                <ul>{this.state.tags.map(tag=><li key={tag}>{tag}</li>)}</ul>
-            </div>
-        );
+        return <div>{this.state.tags.length===0 && "please create new tags"}{this.renderTags()}</div>;
     }
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>There are no Tags!</p>;
 
-    formatCount() {
-        const count = this.state.count;
-        return count === 0 ? "zero" : count;
+        return (
+            <ul>
+                {this.state.tags.map((tag) => (
+                    <li key={tag}>{tag}</li>
+                ))}
+            </ul>
+        );
     }
 }
 
